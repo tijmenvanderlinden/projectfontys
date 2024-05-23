0ddf9dacdd767ddcdd5f1689f8ef076018fabf06
tijmenvanderlinden <tijmenvanderlinden01@gmail.com>
2024-05-23 18:50:21 +0200

    Add files via upload

---
+++ projectfontysparsecchrome.ps1
@@ -0,0 +1,17 @@
+# git links
+$chromeUrl = "https://github.com/tijmenvanderlinden/projectfontys/raw/main/ChromeSetup.exe"
+$parsecUrl = "https://github.com/tijmenvanderlinden/projectfontys/raw/main/parsec-windows%20(1).exe"
+
+# download path
+$downloadPath = "C:\Temp"
+if (-Not (Test-Path $downloadPath)) {
+    New-Item -Path $downloadPath -ItemType Directory
+}
+
+# de download van de installers
+Invoke-WebRequest -Uri $chromeUrl -OutFile "$downloadPath\ChromeSetup.exe"
+Invoke-WebRequest -Uri $parsecUrl -OutFile "$downloadPath\parsec-windows (1).exe"
+
+# installatie van de files
+Start-Process "$downloadPath\ChromeSetup.exe" -ArgumentList "/silent /install" -Wait
+Start-Process "$downloadPath\parsec-windows (1).exe" -ArgumentList "/silent" -Wait
